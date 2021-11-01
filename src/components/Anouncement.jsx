c12e91d9c56aa9d8e45f9fbd76bca55a3661789b
hrj0903 <heerij93@gmail.com>
2021-11-01 23:44:33 +0900

    Navbar design
    announcement update

---
+++ src/components/Anouncement.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Anouncement = () => {
+  return <Container>10만원 이상 구매시 무료배송</Container>;
+};
+
+export default Anouncement;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Anouncement from '../components/Anouncement';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <Anouncement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;

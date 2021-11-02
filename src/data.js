b9ba776e4afcc6e2ea353dae4810628873160db9
hrj0903 <heerij93@gmail.com>
2021-11-02 18:33:48 +0900

    create react slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://i.pinimg.com/564x/65/6a/7b/656a7b13027457c86d7b5e404aa36b6c.jpg',
+    title: '겨울 한정 세일',
+    desc: '추운 겨울 따뜻하게 보내세요 30프로 할인',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://i.pinimg.com/564x/65/6a/7b/656a7b13027457c86d7b5e404aa36b6c.jpg',
+    title: '겨울 한정 세일',
+    desc: '추운 겨울 따뜻하게 보내세요 30프로 할인',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://i.pinimg.com/564x/65/6a/7b/656a7b13027457c86d7b5e404aa36b6c.jpg',
+    title: '겨울 한정 세일',
+    desc: '추운 겨울 따뜻하게 보내세요 30프로 할인',
+    bg: 'fbf0f4',
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Anouncement from '../components/Anouncement';
 import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
 
 const Home = () => {
   return (
     <div>
       <Anouncement />
       <Navbar />
+      <Slider />
     </div>
   );
 };

acdea84de2f71ead0adb4f96b26b5dde40ddf96b
hrj0903 <heerij93@gmail.com>
2021-11-03 00:17:43 +0900

    create categories and list

--- src/data.js
+++ src/data.js
@@ -21,3 +21,56 @@ export const sliderItems = [
     bg: 'fbf0f4',
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://i.pinimg.com/564x/b4/98/e3/b498e39329de185fc3c25b8d03e7ea7c.jpg',
+    title: 'Winter Cardigans',
+  },
+  {
+    id: 2,
+    img: 'https://i.pinimg.com/564x/b4/98/e3/b498e39329de185fc3c25b8d03e7ea7c.jpg',
+    title: 'Winter Cardigans',
+  },
+  {
+    id: 3,
+    img: 'https://i.pinimg.com/564x/b4/98/e3/b498e39329de185fc3c25b8d03e7ea7c.jpg',
+    title: 'Winter Cardigans',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 2,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 3,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 4,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 5,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 6,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 7,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+  {
+    id: 8,
+    img: 'https://i.pinimg.com/236x/7e/76/79/7e76796634417ece35b0e3fa5b90ec17.jpg',
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Anouncement from '../components/Anouncement';
+import Categories from '../components/Categories';
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
+import Products from '../components/Products';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Anouncement />
       <Navbar />
       <Slider />
+      <Categories />
+      <Products />
     </div>
   );
 };

667580e3ef2b2cdb664d462d9218b0423628d3c1
hrj0903 <heerij93@gmail.com>
2021-11-03 19:00:48 +0900

    add login and register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from './pages/Home';
+import Login from './pages/Login';
 import ProductList from './pages/ProductList';
 import Product from './pages/Proudct';
+import Register from './pages/Register';
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://images.pexels.com/photos/6984650/pexels-photo-6984650.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background-color: white;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  font-size: 12px;
+  text-decoration: underline;
+  margin: 5px 0px;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title> 로그인 </Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>로그인</Button>
+          <Link>아이디/패스워드를 잊어버리셨나요?</Link>
+          <Link>새로운 계정 만들기</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
